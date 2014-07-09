79eb32fc8533888d1b16d23e038f5f1fc182c8ca
KIDJourney <kingdeadfish@qq.com>
2014-07-09 15:43:41 +0800

    first time update

---
+++ sokoban/sokoban.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding=utf-8
+import pygame
+from pygame.locals import *
+#Globle Var
+Game_Level = 1
+Game_Map = list()
+Map_Wide = 0
+Map_Deepth = 0
+def ReadMap():
+    Game_Map = []
+    FileName = str(Game_Level)+".dat"
+    file = open(FileName,'r')
+    Map_Deepth,Map_Wide = map(int,file.readline().split())
+    for i in range(Map_Deepth):
+        Game_Map.append(file.readline()[:Map_Wide-1])
+    print Game_Map
+
+if __name__ == "__main__":
+    ReadMap()
---
+++ test_example/file_test/file_input_test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding=utf-8
+import pygame
+from pygame.locals import *
+#Globle Var
+Game_Level = 1
+Game_Map = list()
+Map_Wide = 0
+Map_Deepth = 0
+def ReadMap():
+    Game_Map = []
+    FileName = str(Game_Level)+".dat"
+    file = open(FileName,'r')
+    Map_Deepth,Map_Wide = map(int,file.readline().split())
+    for i in range(Map_Deepth):
+        Game_Map.append(file.readline()[:Map_Wide])
+    print Game_Map
